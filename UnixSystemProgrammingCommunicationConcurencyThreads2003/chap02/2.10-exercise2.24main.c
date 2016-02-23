27f236bd1a6a4aa9a3c9f9b6921d834376eb0c22
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2016-02-23 14:38:20 +0100

    Create 2.10-exercise2.24main.c

---
+++ UnixSystemProgrammingCommunicationConcurencyThreads2003/chap02/2.10-exercise2.24main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#define PATH_DELIMITERS ":"
+int makeargv(const char *s, const char *delimiters, char ***argvp);
+char **getpaths(void) 
+{
+   char **myargv;
+   char *path;
+   path = getenv("PATH");
+   if (makeargv(path, PATH_DELIMITERS, &myargv) == -1)
+      return NULL;
+   else
+      return myargv;
+}
+/*
+gcc -Wall -o 2.10-exercise2.24main 2.6-program2.2makeargv.c 2.10-exercise2.24getpaths.c 2.10-exercise2.24main.c
+*/
