d6a09d5153bbe265155221e4549266a078f63253
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2016-02-23 14:25:55 +0100

    Update 1.6program1.1v2checkpass.sh

--- UnixSystemProgrammingCommunicationConcurencyThreads2003/chap01/1.6program1.1v2checkpass.sh
+++ UnixSystemProgrammingCommunicationConcurencyThreads2003/chap01/1.6program1.1v2checkpass.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+# Prerequisite : there is an executable compiled with the following 
+# gcc -Wall -o 1.6program1.1v2checkpass 1.6program1.1v2checkpass.c 
 basicstr="mypass"
 EXTRAINPUT=(a 6stuff 6stuffa 6stuffb 6stuffc 6stuffz 6stuffA 6stuffZ)
-./1.6prog1.1v2 $basicstr  #this gives success
+./1.6program1.1v2checkpass $basicstr  #this gives success
 for word in ${EXTRAINPUT[*]}
 do
   input=${basicstr}${word}  #all these inputs should not give success...
   # echo "---passing $input for checking --- "
-  ./1.6prog1.1v2 $input  #for first 2 cases they fail , but then again give success
+  ./1.6program1.1v2checkpass $input  #for first 2 cases they fail , but then again give success
 done
