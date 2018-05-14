acc576fd63a2c8fd297abecf60fddc8ba50a7af5
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-05-14 12:31:28 +0200

    Update 2.6-program2.2makeargv.c

--- UnixSystemProgrammingCommunicationConcurencyThreads2003/chap02/2.6-program2.2makeargv.c
+++ UnixSystemProgrammingCommunicationConcurencyThreads2003/chap02/2.6-program2.2makeargv.c
@@ -10,17 +10,17 @@ int makeargv(const char *s, const char *delimiters, char ***argvp)
    if ((s == NULL) || (delimiters == NULL) || (argvp == NULL)) 
 	{ errno = EINVAL; return -1; }
    *argvp = NULL; /* so that a failed call to malloc,will leave it NULL */
-	  /* now we consume any initial delimiters characters of input s */
-   snew = s + strspn(s, delimiters);         /* snew is real start of string */
+	  /* now we consume any initial delimiters characters of input s and move our pointer after them */
+   snew = s + strspn(s, delimiters);       /* snew points to the real start of string skipping any initial delimiters */
    if ((t = malloc(strlen(snew) + 1)) == NULL)
-      return -1;
+      return -1;  /* if allocation of memory for the temporary string failed, terminate program */
    strcpy(t, snew);
    numtokens = 0;
    if (strtok(t, delimiters) != NULL)     /* then count the number of tokens in s */
       for (numtokens = 1; strtok(NULL, delimiters) != NULL; numtokens++) ;
       /* next,create argument array for ptrs to the tokens */
    if ((*argvp = malloc((numtokens + 1)*sizeof(char *))) == NULL) 
-	{
+    {
       error = errno;
       free(t);
       errno = error;
@@ -29,12 +29,12 @@ int makeargv(const char *s, const char *delimiters, char ***argvp)
       /* now insert pointers-to-tokens into the argument array */
    if (numtokens == 0)  free(t);
     else 
-	{
+    {
       strcpy(t, snew);
       **argvp = strtok(t, delimiters);
       for (i = 1; i < numtokens; i++)
-          *((*argvp) + i) = strtok(NULL, delimiters);
+          *((*argvp) + i) = strtok(NULL, delimiters);  
     }
-    *((*argvp) + numtokens) = NULL;             /* append final NULL pointer */
+    *((*argvp) + numtokens) = NULL;             /* append final NULL pointer in the array of pointers-to-tokens */
     return numtokens;
 }
