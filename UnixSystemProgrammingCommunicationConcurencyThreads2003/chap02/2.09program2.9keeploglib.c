3ad78b5ad9fe8d62096877e6ca3ae6c9478d5448
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-05-16 12:08:17 +0200

    Update 2.09program2.9keeploglib.c

--- UnixSystemProgrammingCommunicationConcurencyThreads2003/chap02/2.09program2.9keeploglib.c
+++ UnixSystemProgrammingCommunicationConcurencyThreads2003/chap02/2.09program2.9keeploglib.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "2.09program2.6listlib.h"
 int runproc(char *cmd)  /* execute cmd; store cmd and time in history list */
 {
@@ -9,7 +10,9 @@ int runproc(char *cmd)  /* execute cmd; store cmd and time in history list */
    execute.string = cmd;
    if (system(cmd) == -1)           /* command could not be executed at all? */
       return -1;
-   return adddata(execute);
+   if( strlen(execute.string) != 0 ) /* only append real commands, not the ENTER press of user when he wants to change line */
+      return adddata(execute);
+   return 0;
 }
 void showhistory(FILE *f)        /* output the history list of the file f */
 {
@@ -18,9 +21,10 @@ void showhistory(FILE *f)        /* output the history list of the file f */
    key = accessdata();
    if (key == -1)
 	   { fprintf(f, "No history\n"); return; }
-   while (!getdata(key, &data) && (data.string != NULL))
-	 {
+   while (!getdata(key, &data) && (data.string != NULL) )
+   {
       fprintf(f, "Command: %s\nTime: %s\n", data.string, ctime(&(data.time)));
       free(data.string);
    }
 }
+
