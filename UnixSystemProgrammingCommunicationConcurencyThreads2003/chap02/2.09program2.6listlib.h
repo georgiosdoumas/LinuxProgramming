491e143e28fb5c58a53e2760c1e0ff1a94246599
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-05-16 09:56:11 +0200

    Create 2.09program2.6listlib.h

---
+++ UnixSystemProgrammingCommunicationConcurencyThreads2003/chap02/2.09program2.6listlib.h
@@ -0,0 +1,9 @@
+#include <time.h>
+typedef struct data_struct {
+     time_t time;
+     char *string;	/* a string of not predifined size */
+} data_t;
+int accessdata(void);
+int adddata(data_t data);
+int freekey(int key);
+int getdata(int key, data_t *datap);
